a1622bd7bb623118e58971575133dee57e896e1b
benyamin <benyamin.84.yt@gmail.com>
2024-10-16 11:54:15 +0330

    course table to api

--- src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourse.jsx
+++ src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourse.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import MyCourseTable from './MyCourseTable/MyCourseTable'
 import { getQuery } from '../../../../core/services/api/reactQuery/getQuery'
-
+import { usequery } from '../../../../core/services/api/reactQuery/usequery'
+import { Skeleton } from '@nextui-org/react'
 const MyCourse = () => {
-  getQuery('MyCourses', '/SharePanel/GetMyCourses?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=LastUpdate&Query=')
+  getQuery('MyCourses', '/SharePanel/GetMyCourses?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=LastUpdate');
   return (
     <div>
       <div className='title font-DanaFaNum-700 text-2xl'>دوره من</div>
-      <div className='rounded-2xl bg-gray-100'>
-        <MyCourseTable  />
+      <div className='min-h-20 mt-6'>
+        {usequery("MyCourses") ? <MyCourseTable  /> : <Skeleton className="rounded-2xl w-full min-h-52 h-full"/> }
+        
       </div>
     </div>
   )
--- src/components/common/ButtonSpecial/index.jsx
+++ src/components/common/ButtonSpecial/index.jsx
@@ -1,12 +1,12 @@
 import { Button } from "@nextui-org/react";
 import React from "react";
 
-const ButtonSpecial = ({ className, innerHtml, onClick }) => {
+const ButtonSpecial = ({ className, innerHtml, onClick, isDisabled, type }) => {
   return (
     // <button className={className} onClick={onClick} >
     //   {innerHtml}
     // </button>
-    <Button className={`min-w-0 h-auto ${className}`} onClick={onClick} radius="full">
+    <Button className={`min-w-0 h-auto ${className}`} onClick={onClick} radius="full" isDisabled={isDisabled} type={type}>
       {innerHtml}
     </Button>
   );
