f21d8f4d7d64f102e6cce208bae0ed2e351c2ca4
benyamin <benyamin.84.yt@gmail.com>
2024-10-16 08:43:13 +0330

    create table

--- src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourse.jsx
+++ src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourse.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import MyCourseTable from './MyCourseTable/MyCourseTable'
 
 const MyCourse = () => {
   return (
-    <div>MyCourse</div>
+    <div>
+      <div className='title font-DanaFaNum-700 text-2xl'>دوره من</div>
+      <div className='rounded-2xl bg-gray-100'>
+        <MyCourseTable />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourseTable/data.js
@@ -0,0 +1,61 @@
+const columns = [
+    {name: "نام", uid: "name"},
+    {name: "مدرس", uid: "teacher"},
+    {name: "سطح", uid: "level"},
+    {name: "", uid: "actions"},
+  ];
+  
+  const users = [
+    {
+      id: 1,
+      name: "Tony Reichert",
+      role: "CEO",
+      team: "Management",
+      level: "active",
+      age: "29",
+      avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+      email: "tony.reichert@example.com",
+    },
+    {
+      id: 2,
+      name: "Zoey Lang",
+      role: "Technical Lead",
+      team: "Development",
+      level: "paused",
+      age: "25",
+      avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
+      email: "zoey.lang@example.com",
+    },
+    {
+      id: 3,
+      name: "Jane Fisher",
+      role: "Senior Developer",
+      team: "Development",
+      level: "active",
+      age: "22",
+      avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+      email: "jane.fisher@example.com",
+    },
+    {
+      id: 4,
+      name: "William Howard",
+      role: "Community Manager",
+      team: "Marketing",
+      level: "vacation",
+      age: "28",
+      avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
+      email: "william.howard@example.com",
+    },
+    {
+      id: 5,
+      name: "Kristen Copper",
+      role: "Sales Manager",
+      team: "Sales",
+      level: "active",
+      age: "24",
+      avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
+      email: "kristen.cooper@example.com",
+    },
+  ];
+  
+  export {columns, users};
