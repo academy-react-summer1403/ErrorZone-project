df7fcc2940a751e9e4e019aad7fa0c69be7865f9
benyamin <benyamin.84.yt@gmail.com>
2024-10-18 16:55:08 +0330

    changes on hero section

--- src/components/LandingHolder/HeroSection/HeroSection.jsx
+++ src/components/LandingHolder/HeroSection/HeroSection.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import RightHeroSecPart from "./RightHeroSecPart";
 import MidHeroSecPart from "./MidHeroSecPart";
 import LeftHeroSecPart from "./LeftHeroSecPart";
 
 const HeroSection = () => {
   return (
-    <div className="mx-auto tablet:h-52 h-[460px] grid grid-cols-7 grid-rows-12 tablet:grid-rows-7 tablet:grid-cols-12 place-items-center tablet:mt-40 tablet:-mb-24  ">
-      <RightHeroSecPart />
-      <MidHeroSecPart />
-      <LeftHeroSecPart />
-    </div>
+    <Fragment>
+      <div className="hidden mx-auto tablet:flex justify-evenly items-center mt-10 tablet:mt-20">
+        <RightHeroSecPart />
+        <MidHeroSecPart />
+        <LeftHeroSecPart />
+      </div>
+      <div className="flex flex-col mx-auto tablet:hidden space-y-12 mt-20 tablet:mt-10 w-fit items-center">
+        <MidHeroSecPart />
+        <RightHeroSecPart />
+        <LeftHeroSecPart />
+      </div>
+    </Fragment>
   );
 };
 
