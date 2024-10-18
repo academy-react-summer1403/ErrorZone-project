5c486a74e535056c6e1b04392dd890d7db42dde0
benyamin <benyamin.84.yt@gmail.com>
2024-10-18 15:47:16 +0330

    skeleton of card list

--- src/components/courseList/courseMain/CourseNames/CourseCardSec/CourseCardSec.jsx
+++ src/components/courseList/courseMain/CourseNames/CourseCardSec/CourseCardSec.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import CourseCard from "../../../../common/CourseCard";
 import { useQuery } from "@tanstack/react-query";
+import { usequery } from "../../../../../core/services/api/reactQuery/useQuery";
+import { Card, Skeleton } from "@nextui-org/react";
 
 const CourseCardSec = () => {
-  const { data, isError, isLoading } = useQuery({
-    queryKey: ["corsesByPagination"],
-  });
+  const data = usequery("corsesByPagination");
 
-  if (isError) return <div className="">error</div>;
-  if (isLoading) return <div className="">loading ...</div>;
+  const { courseFilterDtos } = data ? data : false;
 
-  const { courseFilterDtos } = data;
+  const skeletArray = [];
+  
+  for (let i = 0; i < 12; i++) {
+    skeletArray.push({});
+  }
 
   return (
     <div className=" flex flex-wrap justify-between ">
-      {courseFilterDtos?.map((item, index) => {
-        return <CourseCard item={item} key={index} />;
-      })}
+      {courseFilterDtos ? (
+        courseFilterDtos.map((item, index) => {
+          return <CourseCard item={item} key={index} />;
+        })
+      ) : (
+        <div className="flex justify-between flex-wrap gap-6">
+          {skeletArray.map((item, index) => (
+            <Card className="w-80 space-y-5 p-4" radius="lg" key={index}>
+              <Skeleton className="rounded-lg">
+                <div className="h-48 rounded-lg bg-default-300"></div>
+              </Skeleton>
+              <div className="space-y-3">
+                <Skeleton className="w-3/5 rounded-lg">
+                  <div className="h-6 w-3/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                <Skeleton className="w-4/5 rounded-lg">
+                  <div className="h-6 w-4/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                <Skeleton className="w-2/5 rounded-lg">
+                  <div className="h-6 w-2/5 rounded-lg bg-default-300"></div>
+                </Skeleton>
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
