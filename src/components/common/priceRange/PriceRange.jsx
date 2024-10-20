ea70d773754e765a50ddebf2f2ad735dec9f828d
tima <fatemeabbasi0382@gmail.com>
2024-10-20 11:20:00 +0330

    dashboard

--- src/components/common/priceRange/PriceRange.jsx
+++ src/components/common/priceRange/PriceRange.jsx
@@ -1,9 +1,8 @@
-import {cn, Slider} from "@nextui-org/react";
+import { cn, Slider } from "@nextui-org/react";
 
- function PriceRange({onchange}) {
-
+function PriceRange({ onchange }) {
   return (
-<Slider
+    <Slider
       size="md"
       // label="قیمت"
       maxValue={1000000000}
@@ -13,9 +12,10 @@ import {cn, Slider} from "@nextui-org/react";
       onChange={(value) => onchange(value)}
       classNames={{
         base: "max-w-md gap-3",
-        filler: "bg-gradient-to-r from-pink-300 to-cyan-300 dark:from-pink-600 dark:to-cyan-800",
+        filler:
+          "bg-gradient-to-r from-pink-300 to-cyan-300 dark:from-pink-600 dark:to-cyan-800",
       }}
-      renderThumb={({index, ...props}) => (
+      renderThumb={({ index, ...props }) => (
         <div
           {...props}
           className="group p-1 top-1/2 bg-background border-small border-default-200 dark:border-default-400/50 shadow-medium rounded-full cursor-grab data-[dragging=true]:cursor-grabbing"
@@ -25,7 +25,7 @@ import {cn, Slider} from "@nextui-org/react";
               "transition-transform bg-gradient-to-br shadow-small rounded-full w-5 h-5 block group-data-[dragging=true]:scale-80",
               index === 0
                 ? "from-pink-200 to-pink-500 dark:from-pink-400 dark:to-pink-600" // first thumb
-                : "from-cyan-200 to-cyan-600 dark:from-cyan-600 dark:to-cyan-800", // second thumb
+                : "from-cyan-200 to-cyan-600 dark:from-cyan-600 dark:to-cyan-800" // second thumb
             )}
           />
         </div>
