e58efb008ff6e5c83cfb3d2d2b7cd8f394f16c91
benyamin <benyamin.84.yt@gmail.com>
2024-10-29 12:49:57 +0330

    hamburger manue responsive

--- src/components/common/ButtonSpecial/index.jsx
+++ src/components/common/ButtonSpecial/index.jsx
@@ -1,12 +1,12 @@
 import { Button } from "@nextui-org/react";
 import React from "react";
 
-const ButtonSpecial = ({ className, innerHtml, onClick, isDisabled, type }) => {
+const ButtonSpecial = ({ className, innerHtml, onClick, isDisabled, type, onPress }) => {
   return (
     // <button className={className} onClick={onClick} >
     //   {innerHtml}
     // </button>
-    <Button className={`min-w-0 h-auto ${className}`} onClick={onClick} radius="full" isDisabled={isDisabled} type={type}>
+    <Button onPress={onPress}  className={`min-w-0 h-auto ${className}`} onClick={onClick} radius="full" isDisabled={isDisabled} type={type}>
       {innerHtml}
     </Button>
   );
