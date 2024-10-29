f063ee4176765148986aa242d1c0515889dadaea
tima03a <fatemeabbasi0382@gmail.com>
2024-10-29 18:39:03 +0330

    notfound screen

--- src/screens/NotFound/index.jsx
+++ src/screens/NotFound/index.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react'
+import notfoundImage from '../../assets/images/NotFound404/404.jpg'
 
 const NotFound = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [])
   return (
-    <div>NotFound</div>
+    <div className='grid place-content-center place-items-center bg-blackPanel'>
+      <img src={notfoundImage} alt="" />
+    </div>
   )
 }
 
