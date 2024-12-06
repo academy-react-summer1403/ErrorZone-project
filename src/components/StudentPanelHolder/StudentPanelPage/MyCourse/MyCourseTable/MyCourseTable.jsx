9b44ad2389618c6268313f1924a8641a330528e4
tima03a <fatemeabbasi0382@gmail.com>
2024-12-06 17:20:58 +0330

    LandEdit

--- src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourseTable/MyCourseTable.jsx
+++ src/components/StudentPanelHolder/StudentPanelPage/MyCourse/MyCourseTable/MyCourseTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableHeader,
@@ -11,6 +11,8 @@ import {
   Tooltip,
   getKeyValue,
   Skeleton,
+  Pagination,
+  useDisclosure,
 } from "@nextui-org/react";
 import { EditIcon } from "./EditIcon";
 import { DeleteIcon } from "./DeleteIcon";
@@ -19,27 +21,46 @@ import { EyeIcon } from "./EyeIcon";
 
 // import { usequery } from "../../../../../core/services/api/reactQuery/usequery";
 
-
-import { BookDownloadIcon, Cancel01Icon, MoneyAdd01Icon, ViewIcon } from "hugeicons-react";
-import { Link } from "react-router-dom";
+import {
+  BookDownloadIcon,
+  Cancel01Icon,
+  MoneyAdd01Icon,
+  ViewIcon,
+} from "hugeicons-react";
+import { Link, useNavigate } from "react-router-dom";
 import { getQuery } from "../../../../../core/services/api/reactQuery/getQuery";
 import { usequery } from "../../../../../core/services/api/reactQuery/useQuery";
+import PayModal from "../../../../common/PayModal/PayModal";
 
-const MyCourseTable = () => {
+const MyCourseTable = ({ isDashboard }) => {
+  const [page, setPage] = useState(1);
+  const [courseData, setCourseData] = useState();
+
   const columns = [
     { name: "نام", uid: "courseTitle" },
     { name: "مدرس", uid: "fullName" },
     { name: "سطح", uid: "levelName" },
-    { name: "وضعیت پرداختی", uid: "coursePayments" },
+    { name: "اعمال", uid: "actions" },
   ];
-  getQuery(
-    "MyCourses",
-    "/SharePanel/GetMyCourses?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=LastUpdate"
-  );
+  if (isDashboard) {
+    getQuery(
+      "MyCourses",
+      `/SharePanel/GetMyCourses?PageNumber=${page}&RowsOfPage=2&SortingCol=DESC&SortType=LastUpdate`,
+      page
+    );
+  } else {
+    getQuery(
+      "MyCourses",
+      `/SharePanel/GetMyCourses?PageNumber=${page}&RowsOfPage=10&SortingCol=DESC&SortType=LastUpdate`,
+      page
+    );
+  }
 
-  const data = usequery("MyCourses");
+  const data = usequery("MyCourses", page);
 
-  const { listOfMyCourses } = data ? data : [];
+  // const { listOfMyCourses } = data ? data : [];
+  const navigate = useNavigate();
+  const { isOpen, onOpenChange, onOpen } = useDisclosure();
 
   const renderCell = React.useCallback((user, columnKey) => {
     const cellValue = user[columnKey];
@@ -75,63 +96,98 @@ const MyCourseTable = () => {
         );
       case "actions":
         return (
-          <div className="relative flex items-center gap-2">
-            <div>
+          <div className="relative flex justify-center items-center gap-3">
+            <div className="w-fit">
               <Link to={`/courses/${user.courseId}`}>
                 <Tooltip content="مشاهده">
                   <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <ViewIcon />
+                    <ViewIcon color="black" />
                   </span>
                 </Tooltip>
               </Link>
             </div>
+            {data?.listOfMyCourses?.paymentStatus != "پرداخت نشده" ? (
+              <>
+                <div className="w-fit ">
+                  <Tooltip content="پرداخت">
+                    <MoneyAdd01Icon
+                      onClick={() => {
+                        setCourseData(user);
+                        onOpen();
+                      }}
+                    />
+                  </Tooltip>
+                </div>
+              </>
+            ) : (
+              false
+            )}
           </div>
         );
-        case "coursePayments":
-          return(
-            <Chip
-            className="capitalize  text-black"
-            size="sm"
-            variant="flat"
-          >
-            <MoneyAdd01Icon />
-          </Chip>
-          )
       default:
         return cellValue;
-  
-
     }
   }, []);
 
   return (
     <>
-      {listOfMyCourses ? (
-        <Table
-          className="bg-gray-100"
-          aria-label="Example table with custom cells"
-        >
-          <TableHeader className="bg-gray-200" columns={columns}>
-            {(column) => (
-              <TableColumn
-                key={column.uid}
-                align={column.uid === "actions" ? "center" : "start"}
-              >
-                {column.name}
-              </TableColumn>
-            )}
-          </TableHeader>
-          <TableBody items={listOfMyCourses ? listOfMyCourses : []}>
-            {(item) => (
-              <TableRow key={item.courseId}>
-                {(columnKey) => (
-                  <TableCell>{renderCell(item, columnKey)}</TableCell>
-                )}
-              </TableRow>
-            )}
-          </TableBody>
-
-        </Table>
+      {data?.listOfMyCourses ? (
+        <>
+          <Table
+            className="bg-gray-100"
+            aria-label="Example table with custom cells"
+            bottomContent={
+              !isDashboard && (
+                <div
+                  className="flex w-full justify-center"
+                  style={{ direction: "ltr" }}
+                >
+                  <Pagination
+                    // isCompact
+                    loop
+                    showControls
+                    showShadow
+                    color="primary"
+                    page={page}
+                    total={
+                      isDashboard
+                        ? Math.ceil(data?.totalCount / 2)
+                        : Math.ceil(data?.totalCount / 10)
+                    }
+                    onChange={(page) => setPage(page)}
+                  />
+                </div>
+              )
+            }
+          >
+            <TableHeader className="bg-gray-200" columns={columns}>
+              {(column) => (
+                <TableColumn
+                  key={column.uid}
+                  align={column.uid == "actions" ? "center" : "start"}
+                >
+                  {column.name}
+                </TableColumn>
+              )}
+            </TableHeader>
+            <TableBody
+              items={data?.listOfMyCourses ? data?.listOfMyCourses : []}
+            >
+              {(item) => (
+                <TableRow key={item.courseId}>
+                  {(columnKey) => (
+                    <TableCell>{renderCell(item, columnKey)}</TableCell>
+                  )}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <PayModal
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            courseData={courseData}
+          />
+        </>
       ) : (
         <Skeleton className="rounded-2xl w-full min-h-52 h-full" />
       )}
